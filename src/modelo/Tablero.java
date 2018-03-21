b1ae46b8d1cfb9dbd3bd38dbbc1eeb2f5ae5cdb5
Abel Hermoso Espada <abelhermosoespada@gmail.com>
2018-03-21 21:20:10 +0100

    quita switch

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -7,18 +7,7 @@ public class Tablero {
 
 	public Tablero(Dificultad dificultad) {
 		super();
-		int size=0;
-		switch(dificultad) {
-		case facil:
-			size=5;
-			break;
-		case medio:
-			size=15;
-			break;
-		case dificil:
-			size=25;
-			break;
-		}
+		int size=dificultad.getValor();
 		this.cartas=new Carta[size*2];
 		for (int i = 0; i < cartas.length; i++) {
 			cartas[i]=new Carta();
