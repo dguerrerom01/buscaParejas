3a2fa8b61e21bf91d2da482894d524442d092531
Abel Hermoso Espada <abelhermosoespada@gmail.com>
2018-03-21 21:21:11 +0100

    new jugada

--- src/controlador/Accionable.java
+++ src/controlador/Accionable.java
@@ -1,15 +1,23 @@
 package controlador;
 
+import modelo.Tablero;
+
 public interface Accionable {
 
 	/**
 	 * bucle que lleva el curso del juego
 	 */
-	public void game();
+	public void game(Tablero tablero);
 	
 	
 	/**
 	 * funcionamiento del click izquierdo
 	 */
 	public void accionClickIzquierdo();
+
+
+	public void jugada();
+
+
+	
 }
